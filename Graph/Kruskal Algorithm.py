628fce0779baf227f63b3a525ee605ac9ac41e33
SnapFlip20 <jungmin1755@gmail.com>
2021-01-08 19:28:52 +0900

    Graph

---
+++ Graph/Kruskal Algorithm.py
@@ -0,0 +1,36 @@
+# Kruskal Algorithm(for making MST)
+
+import sys
+
+def union(a, b):
+    a, b = find(a), find(b)
+    if a < b:
+        tree[b] = a
+    else:
+        tree[a] = b
+
+def find(n):
+    if tree[n] == n:
+        return n
+    else:
+        tree[n] = find(tree[n])
+        return tree[n]
+
+v, e = map(int, sys.stdin.readline().split())
+tree = {x:x for x in range(v+1)}
+graph = []
+
+for i in range(e):
+    a, b, c = map(int, sys.stdin.readline().split())
+    graph.append((a, b, c))
+
+graph.sort(key = lambda x: x[2])
+
+weight = 0
+for i in graph:
+    a, b, c = i
+    if find(a) != find(b):
+        union(a, b)
+        weight += c
+
+print(weight)
