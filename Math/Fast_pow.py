2bb649f2255d274d7dcbccb0a32b4adcc6bc0998
SnapFlip20 <jungmin1755@gmail.com>
2021-01-19 18:02:38 +0900

    added Fast pow

---
+++ Math/Fast_pow.py
@@ -0,0 +1,24 @@
+# Fast power algorithm
+
+def fast_pow(x, y, mod=None):
+    res = 1
+    while True:
+        if (y == 0):
+            break
+        if (y % 2 == 1):
+            res = (res*x) % mod
+        y //= 2
+        x = (x**2) % mod
+
+    return res
+
+
+def fast_pow2(x, y, mod=None):
+    res = 1
+    x %= mod
+    while y > 0:
+        if y & 1:
+            res = (res * x) % mod
+        y >>= 1
+        x = x * x % mod
+    return res
