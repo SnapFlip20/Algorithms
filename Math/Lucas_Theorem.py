8d977e8e030af5373b2e83ba31b1d3bfe9f21e27
SnapFlip20 <jungmin1755@gmail.com>
2021-02-25 15:28:33 +0900

    added Lucas_Theorem.py

---
+++ Math/Lucas_Theorem.py
@@ -0,0 +1,23 @@
+# Lucas Theorem
+
+MAX = 2000 # m의 범위에 따라 적절하게 조정해주기
+
+def lucas_theorem(n, k, mod):
+    binom = [[0 for _ in range(MAX)] for _ in range(MAX)]
+    for i in range(mod):
+        binom[i][0] = 1
+        for j in range(1, i+1):
+            binom[i][j] = (binom[i-1][j-1] + binom[i-1][j]) % mod
+        
+    res = 1
+    while n or k:
+        res *= binom[n%mod][k%mod]
+        n //= mod
+        k //= mod
+        res %= mod
+
+    return res
+
+n, k, mod = map(int, input().split())
+
+print(lucas_theorem(n, k, mod))
