2a7dc559cde77e6ceb1c6c60059716c2de81c47d
SnapFlip20 <jungmin1755@gmail.com>
2021-11-12 12:10:56 +0900

    GraphSearch

--- Graph/Breadth_First_Search(BFS).py
+++ Graph/Breadth_First_Search(BFS).py
@@ -1,8 +1,21 @@
 # Breadth First Search(BFS)
 
+
+# 1. 기본형
 from collections import deque
 
 def bfs(graph, start, visit):
+    q = deque([start])
+    visit[start] = True
+    while q:
+        v = q.popleft()
+        for i in graph[v]:
+            if not visit[i]:
+                q.append(i)
+                visit[i] = True
+
+# 0. 테스트용
+def _bfs(graph, start, visit):
     q = deque([start])
     visit[start] = True
     while q:
@@ -13,20 +26,33 @@ def bfs(graph, start, visit):
                 q.append(i)
                 visit[i] = True
 
-def _test():
+def test():
+    '''
+         <예시 그래프>
+
+               1 ⟹ 시작
+             ╱ │ ╲
+            2  4  8
+           ╱  ╱ ╲
+          5  3   7
+                ╱
+               6
+    
+    '''
     graph = [
         [],
-        [2, 3, 8],
-        [1, 7],
-        [1, 4, 5],
-        [3, 5],
-        [3, 4],
+        [2, 4, 8],
+        [1, 5],
+        [4],
+        [1, 3, 7],
+        [2],
         [7],
-        [2, 6, 8],
-        [1, 7]
+        [4, 6],
+        [1]
     ]
     visit = [False for i in range(9)]
-    bfs(graph, 1, visit)
+    print('노드 방문 순서:')
+    _bfs(graph, 1, visit)
 
 if __name__ == "__main__":
-    _test()
+    test()
