4f879e8aea165053a5c259907818ea1a9e8db3bb
SnapFlip20 <jungmin1755@gmail.com>
2021-11-16 00:19:51 +0900

    StronglyConnectedComponent

--- Graph/Tarjan.py
+++ Graph/Tarjan.py
@@ -1,4 +1,4 @@
-# Tarjan Algorithm for making Strongly Connected Component
+# Tarjan
 
 '''
 ---list---
@@ -14,9 +14,9 @@ result: 자기자신과 자식 노드들의 dfsn 번호 중 가장 작은 번호
 scc_cnt: SCC 집합의 개수(== len(scc_lst)-1)
 '''
 
-import sys
-
+#import sys
 #sys.setrecursionlimit(10**9)
+
 MAX = 10001
 
 graph = [[] for _ in range(MAX)]
@@ -29,17 +29,16 @@ scc_lst = []
 def dfs(node):
     global cnt
     cnt += 1
-    dfsn[node] = cnt # dfsn 결정
+    dfsn[node] = cnt
     stack.append(node)
     
     result = dfsn[node]
     for i in graph[node]:
-        if dfsn[i] == 0: # 아직 방문하지 않았을 경우
+        if dfsn[i] == 0:
             result = min(result, dfs(i))
-        elif not finished[i]: # 방문은 했으나 SCC가 형성되지 않았을 경우
+        elif not finished[i]:
             result = min(result, dfsn[i])
 
-    # SCC 추출
     if result == dfsn[node]:
         current_scc = []
         while True:
@@ -54,9 +53,9 @@ def dfs(node):
     return result        
 
 def main():
-    v, e = map(int, sys.stdin.readline().split())
+    v, e = map(int, input().split())
     for i in range(e):
-        a, b = map(int, sys.stdin.readline().split())
+        a, b = map(int, input().split())
         graph[a].append(b)
 
     for i in range(v):
@@ -68,7 +67,7 @@ def main():
     scc_cnt = len(scc_lst)-1
     print(scc_cnt)
     for i in scc_lst[1:]:
-        print(*i, end = ' -1\n')
+        print(*i)
     
 if __name__ == "__main__":
     main()
