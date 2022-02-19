7df0bffe8bbcd6405760b776a254a90efec46265
SnapFlip20 <snapflip20@gmail.com>
2022-02-19 12:06:45 +0900

    NumberTheory

--- Math/Pollard's_Rho.py
+++ Math/Pollard's_Rho.py
@@ -1,7 +1,23 @@
 # Pollard's Rho Algorithm
 
-from math import gcd
+import sys
+from math import gcd, sqrt
 from random import randrange
+
+def MillerRabin(num, prime):
+    s = num-1
+    cnt = 0
+    while s & 1 == 0:
+        s >>= 1
+        cnt += 1
+    p = pow(prime, s, num)
+    if p == 1 or p == num-1:
+        return True
+    for i in range(cnt-1):
+        p = pow(p, 2, num)
+        if p == num-1:
+            return True
+    return False
 
 def PollardRho(num):
     if isPrime(num):
@@ -23,3 +39,12 @@ def PollardRho(num):
         return d
     else:
         return PollardRho(d)
+   
+def isPrime(x):
+    primeList = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41]
+    if x in primeList:
+        return True
+    for i in primeList:
+        if not MillerRabin(x, i):
+            return False
+    return True
