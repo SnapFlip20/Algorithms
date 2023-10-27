1474d8630a9919a38aca1956f0151a97ff3dc369
SnapFlip20 <snapflip20@gmail.com>
2023-10-27 22:05:26 +0900

    DP Opt

--- Optimization/Slope Trick.py
+++ Optimization/Slope Trick.py
@@ -1,18 +1,37 @@
 # Slope Trick
 
-from queue import PriorityQueue
+'''
+n = 7
+a_i = 9 4 8 20 14 15 18 (input example)
+
+b_i = 6 7 8 13 14 15 18 (output example)
+'''
+
+from heapq import heappush, heappushpop
 
 def slope_trick(n, lst):
+    pq = []; seq = []
     res = 0
-    pq = PriorityQueue()
-    for i in range(n):
-        lst_i = lst[i]
-        lst_i -= i
-        if (not pq.empty() and pq[-1] > lst_i):
-            pq.put(lst_i)
-            res += pq[-1] - lst_i
-            pq.pop()
-            pq.put(lst_i)
-        else:
-            pq.put(lst_i)
-    return res
+    
+    for (i, j) in enumerate(ai):
+        a = j-i
+        heappush(pq, -a)
+        top = -pq[0]
+        if pq and top > a:
+            res += top - a
+            heappushpop(pq, -a)
+        seq.append(top)
+    
+    for i in range(n-2, -1, -1):
+        if seq[i] > seq[i+1]:
+            seq[i] = seq[i+1]
+
+    for (i, j) in enumerate(seq):
+        print(i + j)
+
+
+
+n = int(input())
+ai = map(int, input().split())
+
+slope_trick(n, ai)
