125e5e242e9b76bd59d5e84a5e121906c7dd16f8
SnapFlip20 <snapflip20@gmail.com>
2024-03-31 02:08:43 +0900

    Flow

--- Graph/Edmonds Karp.py
+++ Graph/Edmonds Karp.py
@@ -4,51 +4,38 @@ from collections import deque
 
 # ------------------------------------- #
 max_v = 100
-inf = 10**10
+inf = float('inf')
 c = [[0 for _ in range(max_v)] for _ in range(max_v)]
 f = [[0 for _ in range(max_v)] for _ in range(max_v)]
 adj = [[] for _ in range(max_v)]
-total = 0
-flow = inf
 s, e = 0, 1
 # ------------------------------------- #
 
 def bfs(prev, s, e):
-    q = deque()
-    q.append(s)
+    q = deque([s])
+    p = [-1 for _ in range(max_v)]
     while q:
-        if prev[e] != -1:
-            break
         x = q.popleft()
         for i in adj[x]:
-            if c[x][i] - f[x][i] > 0 and prev[i] == -1:
+            if c[x][i] - f[x][i] > 0 and p[i] == -1:
                 q.append(i)
-                prev[i] = x
+                p[i] = x
                 if i == e:
-                    break
-    if prev[e] == -1:
-        return True
-    else:
-        return False
+                    return p
 
 def edmonds_karp(s, e):
-    global flow, total
-    while True:
-        prev = [-1 for _ in range(max_v)]
-        if bfs(prev, s, e):
-            break
-        
+    total = 0
+    while prev := bfs(s, e):
+        flow = inf
         i = e
         while i != s:
             flow = min(flow, c[prev[i]][i]-f[prev[i]][i])
             i = prev[i]
-        
         i = e
         while i != s:
             f[prev[i]][i] += flow
             f[i][prev[i]] -= flow
             i = prev[i]
-
         total += flow
     
     return total
@@ -59,7 +46,8 @@ s, e = map(int, input().split())
 
 for i in range(m):
     u, v, w = map(int, input().split()) # u -> v, weight
-    c[u][v] = 1
-    adj[u].append(v); adj[v].append(u)
+    c[u][v] = w
+    adj[u].append(v)
+    adj[v].append(u)
 
 print(edmonds_karp(s, e))
